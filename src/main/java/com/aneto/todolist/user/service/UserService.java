8b9c54a521e66fd45cad03ba11cc402fb846bec1
Nethanos <n.santos1897@gmail.com>
2021-04-18 00:51:37 -0300

    [feat][wip]: creating userService

--- src/main/java/com/aneto/todolist/user/service/UserService.java
+++ src/main/java/com/aneto/todolist/user/service/UserService.java
@@ -1,10 +1,12 @@
-package com.aneto.todolist.user;
+package com.aneto.todolist.user.service;
 
 import com.aneto.todolist.user.domain.User;
 import org.springframework.stereotype.Service;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -14,6 +16,24 @@ public class UserService {
 
     public UserService(EntityManager em) {
         this.em = em;
+    }
+
+    public User findByUsername(String username){
+        String sql = "select u from User u where u.username = :username";
+        TypedQuery<User> query = em.createQuery(sql, User.class);
+
+        query.setParameter("username", username);
+
+        return query.getSingleResult();
+    }
+
+    public User findById(String id) {
+
+        Optional<User> user = Optional.of(em.find(User.class, id));
+        /**
+         * TODO: Tratar da forma correta
+         */
+        return user.orElseThrow(() -> new RuntimeException("batata"));
     }
 
 
