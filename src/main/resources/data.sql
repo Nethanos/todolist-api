da76187e04ce2d6262123a7bf3452a96de9f5121
Nethanos <n.santos1897@gmail.com>
2021-04-18 19:12:27 -0300

    [feat]: adding a third test role

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,9 +1,12 @@
 INSERT INTO db_user (id, username, email, password) values
 ('1', 'johnDoe', 'jd@gmail.com', '$2y$12$Isu5h7OrU66btes2J0bol.kqutrlwz..JnvVS85okS3E/7/iDnOj.'),
-('2', 'jack', 'jackd@gmail.com', '$2y$12$Isu5h7OrU66btes2J0bol.kqutrlwz..JnvVS85okS3E/7/iDnOj.');
+('2', 'jack', 'jackd@gmail.com', '$2y$12$Isu5h7OrU66btes2J0bol.kqutrlwz..JnvVS85okS3E/7/iDnOj.'),
+('3', 'visitor', 'visitant@gmail.com', '$2y$12$Isu5h7OrU66btes2J0bol.kqutrlwz..JnvVS85okS3E/7/iDnOj.');
 
 
 insert into db_role values ('1', 'USER');
 insert into db_role values ('2', 'ADMIN');
+insert into db_role values ('3', 'GUEST');
 
-insert into users_roles (user_id, role_id) values ('1', '1'), ('2', '1');
+
+insert into users_roles (user_id, role_id) values ('1', '1'), ('2', '1'), ('3', '3');
