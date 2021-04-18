4cdb8e6c9f6e2fedd4f008f46b6dc287dfe1803a
Nethanos <n.santos1897@gmail.com>
2021-04-18 00:37:08 -0300

    [refactor][db]: changing previous inserted users

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,6 @@
-INSERT INTO db_user values ('1', 'johnDoe', 'jd@gmail.com', '12324');
-INSERT INTO db_user values ('2', 'jack', 'jackd@gmail.com', '65432');
+INSERT INTO db_user (id, username, email, password) values
+('1', 'johnDoe', 'jd@gmail.com', '$2y$12$Isu5h7OrU66btes2J0bol.kqutrlwz..JnvVS85okS3E/7/iDnOj.'),
+('2', 'jack', 'jackd@gmail.com', '$2y$12$Isu5h7OrU66btes2J0bol.kqutrlwz..JnvVS85okS3E/7/iDnOj.');
 
 
 insert into db_role values ('1', 'USER');
