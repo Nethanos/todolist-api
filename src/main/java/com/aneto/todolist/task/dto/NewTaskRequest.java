e2245b8740ac1c259c0260d1cc8be9b0036a41d9
Nethanos <n.santos1897@gmail.com>
2021-04-18 14:15:45 -0300

    [refactor]

--- src/main/java/com/aneto/todolist/task/dto/NewTaskRequest.java
+++ src/main/java/com/aneto/todolist/task/dto/NewTaskRequest.java
@@ -1,10 +1,13 @@
 package com.aneto.todolist.task.dto;
 
 import com.aneto.todolist.task.domain.Task;
-import com.aneto.todolist.utils.JsonDefaultConfig;
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
 
-@JsonDefaultConfig
-public class NewTaskRequest {
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)public class NewTaskRequest {
 
     private String description;
 
