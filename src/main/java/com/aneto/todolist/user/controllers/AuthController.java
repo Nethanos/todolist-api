cfd99d79bd2d75380fc14d3e44fbc5d05437b8f5
AdeildoNeto <n.santos1897@gmail.com>
2021-04-19 16:51:00 -0300

    [documentation]: adding swagger

--- src/main/java/com/aneto/todolist/user/controllers/AuthController.java
+++ src/main/java/com/aneto/todolist/user/controllers/AuthController.java
@@ -3,6 +3,8 @@ package com.aneto.todolist.user.controllers;
 
 import com.aneto.todolist.core.services.JwtTokenService;
 import com.aneto.todolist.user.dto.LoginFormRequest;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -11,6 +13,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.persistence.NoResultException;
 
 @RestController
 @RequestMapping("/auth")
@@ -23,12 +27,18 @@ public class AuthController {
         this.authenticationManager = authenticationManager;
         this.jwtTokenService = jwtTokenService;
     }
+    @ApiOperation(value = "Authenticate a User")
     @PostMapping("")
     public ResponseEntity<String> login(@RequestBody LoginFormRequest loginFormRequest){
 
         UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = loginFormRequest.toAuthToken();
+        Authentication authentication = null;
+        try {
+            authentication = authenticationManager.authenticate(usernamePasswordAuthenticationToken);
+        } catch (Exception ex){
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Usuário incorreto ou não encontrado. Por favor, verifique as credenciais");
+        }
 
-        Authentication authentication = authenticationManager.authenticate(usernamePasswordAuthenticationToken);
         String token = jwtTokenService.generateFullToken(authentication);
         return ResponseEntity.ok(token);
     }
