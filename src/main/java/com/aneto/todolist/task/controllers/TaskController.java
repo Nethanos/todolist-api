f817a58a638ba8c13991c9c36cf92a24d8e2a035
AdeildoNeto <n.santos1897@gmail.com>
2021-04-19 16:58:11 -0300

    [feat]: add get all tasks operation controller

--- src/main/java/com/aneto/todolist/task/controllers/TaskController.java
+++ src/main/java/com/aneto/todolist/task/controllers/TaskController.java
@@ -5,6 +5,7 @@ import com.aneto.todolist.task.dto.NewTaskRequest;
 import com.aneto.todolist.task.dto.TaskResponse;
 import com.aneto.todolist.task.dto.UpdateTaskRequest;
 import com.aneto.todolist.task.services.TaskService;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -29,12 +30,14 @@ public class TaskController {
         this.taskService = taskService;
     }
 
+    @ApiOperation(value = "Retrieve one task")
     @GetMapping("/{id}")
     public ResponseEntity<TaskResponse> retrieveTask(@PathVariable String id){
 
         return ResponseEntity.ok(new TaskResponse(taskService.retrieveTask(id)));
     }
 
+    @ApiOperation(value = "Get a list of tasks")
     @GetMapping
     public ResponseEntity<List<TaskResponse>> getTasks(@RequestParam(name = "status", required = false) String status){
         List<Task> taskList = taskService.getTasks(status);
@@ -46,6 +49,18 @@ public class TaskController {
         return ResponseEntity.ok(taskResponseList);
     }
 
+    @GetMapping("/all")
+    public ResponseEntity<List<TaskResponse>> getAllTasks(@RequestParam(name = "status", required = false) String status){
+        List<Task> taskList = taskService.getAllTasks(status);
+
+        ArrayList<TaskResponse> taskResponseList = new ArrayList<>();
+
+        taskList.forEach((task) -> taskResponseList.add(new TaskResponse(task)));
+
+        return ResponseEntity.ok(taskResponseList);
+    }
+
+    @ApiOperation(value = "Create a new Task")
     @PostMapping
     public ResponseEntity<String> newTask(@RequestBody NewTaskRequest newTaskRequest) {
 
@@ -56,6 +71,7 @@ public class TaskController {
         return ResponseEntity.status(HttpStatus.CREATED).body(taskId);
     }
 
+    @ApiOperation(value = "Update a task")
     @PutMapping("/{id}")
     public ResponseEntity updateTask(@RequestBody UpdateTaskRequest updateTaskRequest, @PathVariable String id){
         taskService.updateTask(updateTaskRequest.toModel(), id);
@@ -63,6 +79,7 @@ public class TaskController {
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 
+    @ApiOperation(value = "Delete a task")
     @DeleteMapping("/{id}")
     public ResponseEntity deleteTask(@PathVariable String id){
         taskService.deleteTask(id);
