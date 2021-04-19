25ec1951efafe1b4349fa6e5fc9b11b0365d47eb
AdeildoNeto <n.santos1897@gmail.com>
2021-04-19 16:59:55 -0300

    [feat]: apiError

---
+++ src/main/java/com/aneto/todolist/core/exceptions/ApiError.java
@@ -0,0 +1,17 @@
+package com.aneto.todolist.core.exceptions;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+@JsonInclude(JsonInclude.Include.NON_EMPTY)
+@JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
+public class ApiError {
+
+    private final String field;
+    private final String errorMessage;
+
+    public ApiError (String field, String errorMessage) {
+        this.field = field;
+        this.errorMessage = errorMessage;
+    }
+}
