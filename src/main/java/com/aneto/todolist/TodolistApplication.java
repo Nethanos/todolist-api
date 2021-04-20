967b0b7357ad98a9196b3c5e1b055d600e9d452c
Nethanos <n.santos1897@gmail.com>
2021-04-20 07:13:04 -0300

    [feat][wip]:adding logger

--- src/main/java/com/aneto/todolist/TodolistApplication.java
+++ src/main/java/com/aneto/todolist/TodolistApplication.java
@@ -1,13 +1,23 @@
 package com.aneto.todolist;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TodolistApplication {
 
+	static final Logger log =
+			LoggerFactory.getLogger(TodolistApplication.class);
+
 	public static void main(String[] args) {
+		log.info("Before Starting application");
+
 		SpringApplication.run(TodolistApplication.class, args);
+
+		log.debug("Starting todo list in debug with {} args", args.length);
+		log.info("Starting todo list with {} args.", args.length);
 	}
 
 }
